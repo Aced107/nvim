dc99e987e7cdeb49338cf823a4aea9197e441ec6
Aced107 <mohammad.armaan107@gmail.com>
2024-05-16 16:38:50 +0200

    Updated LSP

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -156,6 +156,7 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        html = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -166,7 +167,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
